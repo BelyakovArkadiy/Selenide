679bbbf2c3b07ee7b5d059af1c01bf775f393411
BelyakovArkadiy <belyakovarkadiy@gmail.com>
2021-03-06 10:47:08 +0600

    modified:   src/test/java/ru/netology/web/CallbackTest.java

--- src/test/java/ru/netology/web/CallbackTest.java
+++ src/test/java/ru/netology/web/CallbackTest.java
@@ -11,11 +11,10 @@ class CallbackTest {
     @Test
     void shouldTest() {
         open("http://localhost:9999");
-        SelenideElement form = $("[data-test-id=callback-form]");
-        form.$("[data-test-id=name] input").setValue("Василий");
-        form.$("[data-test-id=phone] input").setValue("+79270000000");
-        form.$("[data-test-id=agreement]").click();
-        form.$("[data-test-id=submit]").click();
+        $("[data-test-id=name] input").setValue("Василий");
+        $("[data-test-id=phone] input").setValue("+79270000000");
+        $("[data-test-id=agreement]").click();
+        $("[data-test-id=submit]").click();
         $(".alert-success").shouldHave(exactText("Ваша заявка успешно отправлена!"));
     }
 }
